0299de1f465c7b68aacba4250f24fd846dcbc394
Uche Okoroafor <uche@magnitt.com>
2024-10-16 19:47:18 +0400

    Add robots.txt and setupTests.js files
    - Added robots.txt file to build and public directories to specify rules for web crawlers.
    - Added setupTests.js file to src directory for custom jest matchers.
    
    Closes: #123

---
+++ src/endpoints.js
@@ -0,0 +1,6 @@
+// export const  SERVER_URL='http://localhost:3233'
+// export const  SOCKET_IO_URL='http://localhost:3234'
+
+export const SERVER_URL = 'http://13.53.39.180:3233'
+export const SOCKET_IO_URL = 'http://13.53.39.180:3234'
+console.log('******** Hello World ********')
