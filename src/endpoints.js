2e8e5398dfafaa51013b999806ea34fb7381912b
Uche Okoroafor <uche@magnitt.com>
2024-10-18 21:36:03 +0400

    Update server URL and socket IO URL in endpoints.js

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,5 @@
 // export const  SERVER_URL='http://localhost:3233'
 // export const  SOCKET_IO_URL='http://localhost:3234'
 
-export const SERVER_URL = 'http://13.53.39.180:3233'
-export const SOCKET_IO_URL = 'http://13.53.39.180:3234'
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL 
+export const SOCKET_IO_URL = process.env.REACT_APP_SOCKET_IO_URL
