6427a4ad8bc66a152752a341066df611f6988b6c
Uche Okoroafor <uche@magnitt.com>
2024-10-18 15:47:24 +0400

    Update server URL in endpoints.js

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,5 @@
 // export const  SERVER_URL='http://localhost:3233'
 // export const  SOCKET_IO_URL='http://localhost:3234'
 
-export const SERVER_URL = 'http://13.53.39.180'
+export const SERVER_URL = 'https://alcatraz-trade.netlify.app/api'
 export const SOCKET_IO_URL = 'http://13.53.39.180:3234'
