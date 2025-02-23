618cae49a3f082753387484621b2671127520547
Uche Okoroafor <uche@magnitt.com>
2025-02-23 15:32:04 +0400

    Update endpoint URLs and asset manifest for new main.js file, and add license for main.js

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,6 +1,6 @@
-// export const  SERVER_URL='http://localhost:3233'
-// export const  SOCKET_IO_URL='http://localhost:3234'
+export const  SERVER_URL='http://localhost:3233'
+export const  SOCKET_IO_URL='http://localhost:3234'
 
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL 
-export const SOCKET_IO_URL = process.env.REACT_APP_SOCKET_IO_URL
+// export const SERVER_URL = process.env.REACT_APP_SERVER_URL 
+// export const SOCKET_IO_URL = process.env.REACT_APP_SOCKET_IO_URL
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
