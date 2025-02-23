be05e2dde471b8a6997e14e0b53283a9088b629c
Uche Okoroafor <uche@magnitt.com>
2025-02-23 16:10:21 +0400

    Refactor endpoint URLs to use environment variables and comment out hardcoded values

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,6 +1,6 @@
-export const  SERVER_URL='http://13.61.246.78:3233'
-export const  SOCKET_IO_URL='http://13.61.246.78:3234'
+// export const  SERVER_URL='http://13.61.246.78:3233'
+// export const  SOCKET_IO_URL='http://13.61.246.78:3234'
 
-// export const SERVER_URL = process.env.REACT_APP_SERVER_URL 
-// export const SOCKET_IO_URL = process.env.REACT_APP_SOCKET_IO_URL
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL 
+export const SOCKET_IO_URL = process.env.REACT_APP_SOCKET_IO_URL
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
